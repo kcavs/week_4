bb234086a1c5df1e6d43d4687e8b6c33200c2b61
kcavs <kcav1321@gmail.com>
2024-02-26 14:36:51 -0500

    rect!!

---
+++ rect.java
@@ -0,0 +1,19 @@
+/***
+ * project: rectangle
+ * programmer: kiersten cavalli
+ * date: feb 26th, 2023
+ * program: rect.java
+ ***/
+public class rect
+{
+    public static void main (String[]args)
+    {
+        System.out.println("***************");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("*             *");
+        System.out.println("***************");
+    }
+}
