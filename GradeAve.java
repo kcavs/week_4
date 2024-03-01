9267d48bd95acfe5a152e557c726f091d1a9dd35
kcavs <kcav1321@gmail.com>
2024-03-01 14:22:02 -0500

    hi

---
+++ GradeAve.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class GradeAve {
+    public static void main(String[]args){
+        int grade1;
+        int grade2;
+        int grade3;
+        int grade4;
+        int grade5;
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your first grade: ");
+        grade1= input.nextInt();
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your second grade: ");
+        grade1= input.nextInt();
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your third grade: ");
+        grade1= input.nextInt();
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your fourth grade: ");
+        grade1= input.nextInt();
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter your fith grade: ");
+        grade1= input.nextInt();
+        int average;
+        average= ((grade1))
+    }
+}
